ee019c719958cd9750fbe033831f5e96cae77ffb
Dave Thomas <davidcthomas@fb.com>
2019-12-25 13:40:10 -0800

    experts per site template

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,27 @@ from flask_restful import reqparse
 so = stackoverflow.StackOverflow()
 parser = reqparse.RequestParser()
 
+
+def get_page(args):
+    if args.page is None:
+        page = int(1)
+    else:
+        page = int(args.page)
+        if not isinstance(page, int):
+            page = int(1)
+    return page
+
+
+def get_pagesize(args):
+    if args.pagesize is None:
+        pagesize = int(15)
+    else:
+        pagesize = int(args.pagesize)
+        if not isinstance(pagesize, int):
+            pagesize = int(15)
+    return pagesize
+
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -18,21 +39,37 @@ def sites():
     parser.add_argument('pagesize', location='args')
 
     args = parser.parse_args()
-    if args.page is None:
-        page = 1
-    else:
-        page = int(args.page)
-        if not isinstance(page, int):
-            page = 1
 
-    if args.pagesize is None:
-        page = 1
+    return_dict = so.get_sites(get_page(args), get_pagesize(args))
+    if "error" in return_dict:
+        return render_template('error.html', error=return_dict["error"], url=return_dict["url"])
     else:
-        page = int(args.page)
-        if not isinstance(page, int):
-            page = 1
-    return_dict = so.get_sites(page)
-    return render_template('sites.html', return_dict=return_dict)
+        return render_template('sites.html', title="Top Sites", return_dict=return_dict)
+
+
+@app.route('/users_rep')
+def users_rep():
+    parser.add_argument('page', location='args')
+    parser.add_argument('pagesize', location='args')
+    parser.add_argument('site', location='args')
+    parser.add_argument('name', location='args')
+
+    args = parser.parse_args()
+    if args.name is None:
+        name = ""
+    else:
+        name = args.name
+
+    if args.site is None:
+        site = ""
+    else:
+        site = args.site
+
+    return_dict = so.get_top_users_reputation(site, get_page(args), get_pagesize(args))
+    if "error" in return_dict:
+        return render_template('error.html', error=return_dict["error"], url=return_dict["url"])
+    else:
+        return render_template('top_rep.html', title="Top Experts", name=name, site=site, return_dict=return_dict)
 
 
 @app.route('/comments')
--- app/stackoverflow.py
+++ app/stackoverflow.py
@@ -1,8 +1,10 @@
-from google.cloud import bigquery
-from stackapi import StackAPI
+from urllib import parse
+# from google.cloud import bigquery
+# from stackapi import StackAPI
 import requests
 import json
-SO_DEFAULT_PAGESIZE = 15
+SO_DEFAULT_PAGESIZE = int(15)
+
 
 class StackOverflow:
     def __init__(self):
@@ -12,31 +14,71 @@ class StackOverflow:
         self.so_client_id = 16909
         self.so_api_key = "fBn0lLrgtPLjJeot2NKAbw(("
         self.dict_results = {}
-        self.site = StackAPI('stackoverflow')
+        # self.site = StackAPI('stackoverflow')
         self.s = requests.Session()
         self._so_headers = {}
         self.so_default_pagesize = SO_DEFAULT_PAGESIZE
         self.so_params_dict = {"key": self.so_api_key, "pagesize": self.so_default_pagesize}
+        self.so_api_url = "https://api.stackexchange.com/2.2/"
+        self.so_url_sites = "sites"
+        self.so_url_users_reputation = "users?order=desc&sort=reputation"
 
     def get_sites(self, page, pagesize=SO_DEFAULT_PAGESIZE):
-        so_params_dict = self.so_params_dict
+        so_params_dict = {}
+        so_params_dict.update(self.so_params_dict)
         if isinstance(page, int):
             so_params_dict["page"] = page
         if isinstance(pagesize, int):
             so_params_dict["pagesize"] = pagesize
 
-        r = self.s.get("https://api.stackexchange.com/2.2/sites", params=so_params_dict, headers=self._so_headers)
+        r = self.s.get(self.so_api_url + self.so_url_sites, params=so_params_dict, headers=self._so_headers)
         if r.status_code != 200:
-            return r.status_code
+            return {"error": r.status_code,
+                    "url": {"method": self.so_api_url + self.so_url_sites, "params": so_params_dict}}
         else:
             r_json = r.json()
             item_list = r_json["items"]
             list_dict_sites = []
             return_dict = {}
             for item in item_list:
-                list_dict_sites.append({"name": item["name"], "link": item["site_url"]})
+                list_dict_sites.append(
+                    {"name": item["name"], "name_urlencoded": parse.quote(item["name"]),
+                     "site": item["api_site_parameter"], "link": item["site_url"]})
             return_dict["items"] = list_dict_sites
             return_dict["length"] = len(list_dict_sites)
+            return_dict["page"] = int(page)
+            return_dict["pagesize"] = int(pagesize)
+            if "has_more" in r_json:
+                return_dict["has_more"] = r_json["has_more"]
+            else:
+                return_dict["has_more"] = r_json["has_more"]
+
+            return return_dict
+
+    def get_top_users_reputation(self, site, page, pagesize=SO_DEFAULT_PAGESIZE):
+        so_params_dict = {}
+        so_params_dict.update(self.so_params_dict)
+        if isinstance(page, int):
+            so_params_dict["page"] = page
+        if isinstance(pagesize, int):
+            so_params_dict["pagesize"] = pagesize
+
+        so_params_dict["site"] = site
+
+        r = self.s.get(self.so_api_url + self.so_url_users_reputation, params=so_params_dict, headers=self._so_headers)
+        if r.status_code != 200:
+            return {"error": r.status_code,
+                    "url": {"method": self.so_api_url + self.so_url_users_reputation, "params": so_params_dict}}
+        else:
+            r_json = r.json()
+            item_list = r_json["items"]
+            list_dict_users = []
+            return_dict = {}
+            for item in item_list:
+                list_dict_users.append(
+                    {"name": item["display_name"], "link": item["link"], "reputation": item["reputation"]})
+            return_dict["items"] = list_dict_users
+            return_dict["length"] = len(list_dict_users)
             return_dict["page"] = page
             return_dict["pagesize"] = pagesize
             if "has_more" in r_json:
@@ -46,27 +88,26 @@ class StackOverflow:
 
             return return_dict
 
-    def get_comments(self):
-        self.comments = self.site.fetch('comments')
-        return self.comments
+    # def get_comments(self):
+    #     return self.site.fetch('comments')
 
-    def query_stackoverflow(self):
-        if "results" not in self.dict_results.keys():
-            client = bigquery.Client(project='expert-network-262703')
-
-            query_job = client.query("""
-                SELECT
-                  CONCAT(
-                    'https://stackoverflow.com/questions/',
-                    CAST(id as STRING)) as url,
-                  view_count
-                FROM `bigquery-public-data.stackoverflow.posts_questions`
-                WHERE tags like '%google-bigquery%'
-                ORDER BY view_count DESC
-                LIMIT 10""")
-
-            # results = query_job.result()  # Waits for job to complete.
-            self.dict_results["results"] = query_job
-
-        return self.dict_results["results"]
+    # def query_stackoverflow(self):
+    #     if "results" not in self.dict_results.keys():
+    #         client = bigquery.Client(project='expert-network-262703')
+    #
+    #         query_job = client.query("""
+    #             SELECT
+    #               CONCAT(
+    #                 'https://stackoverflow.com/questions/',
+    #                 CAST(id as STRING)) as url,
+    #               view_count
+    #             FROM `bigquery-public-data.stackoverflow.posts_questions`
+    #             WHERE tags like '%google-bigquery%'
+    #             ORDER BY view_count DESC
+    #             LIMIT 10""")
+    #
+    #         # results = query_job.result()  # Waits for job to complete.
+    #         self.dict_results["results"] = query_job
+    #
+    #     return self.dict_results["results"]
 
