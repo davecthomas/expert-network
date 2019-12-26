3711dde1623c67c0e6e7280dcb0d5e250bea7c96
Dave Thomas <davidcthomas@fb.com>
2019-12-25 17:45:22 -0800

    refactoring to get reputation in the right place

--- app/stackoverflow.py
+++ app/stackoverflow.py
@@ -25,6 +25,7 @@ class StackOverflow:
         self.so_url_sites = "sites"
         self.so_url_users_reputation = "users?order=desc&sort=reputation"
         self.so_filter_include_total = "!9Z(-x-Q)8"  # add &filter= to include a count of all results
+        self.so_current_page_sites_dict = None
 
     def get_sites(self, page, pagesize=SO_DEFAULT_PAGESIZE):
         so_params_dict = {}
@@ -46,14 +47,25 @@ class StackOverflow:
             return_dict = {}
             first = True
             for item in item_list:
-                return_dict_top100_users = self.get_top_users_reputation(
-                    item["api_site_parameter"], page=1, pagesize=100)
-                if "items" in return_dict_top100_users:
-                    df_temp = pd.DataFrame(return_dict_top100_users["items"])
-                    df_temp["site"] = item["api_site_parameter"]
-                    top100_rep_sum = df_temp["reputation"].sum()
-                    df_temp["reputation_ratio"] = df_temp["reputation"] / top100_rep_sum
-                    df_sites_top100_users = df_sites_top100_users.append(df_temp)
+                # For each site in this page, get the top 100 users, by reputation, and calculate each person's reputation ratio
+                r2 = self.s.get(self.so_api_url + self.so_url_users_reputation,
+                                params={"key": self.so_api_key,"page": 1, "pagesize": 100, "site": item["api_site_parameter"]},
+                                headers=self._so_headers)
+                if r2.status_code != 200:
+                    return {"error": r2.status_code,
+                            "url": {"method": self.so_api_url + self.so_url_users_reputation, "params": {"key": self.so_api_key,"page": 1, "pagesize": 100, "site": item["api_site_parameter"]}}}
+                else:
+                    return_dict_top100_users = r2.json()
+                    # return_dict_top100_users = self.get_top_users_reputation( item["api_site_parameter"], page=1, pagesize=100)
+                    if "items" in return_dict_top100_users:
+                        df_temp = pd.DataFrame(return_dict_top100_users["items"])
+                        df_list =["display_name", "reputation", "link", "user_id"]
+                        df_temp = df_temp[df_list]
+                        df_temp["site"] = item["api_site_parameter"]
+                        top100_rep_sum = df_temp["reputation"].sum()
+                        df_temp["reputation_ratio"] = df_temp["reputation"] / top100_rep_sum
+                        df_sites_top100_users = df_sites_top100_users.append(df_temp)
+                        print(df_sites_top100_users)
 
                 list_dict_sites.append(
                     {"name": item["name"], "name_urlencoded": parse.quote(item["name"]),
@@ -61,7 +73,8 @@ class StackOverflow:
 
             df_sites_top100_users = df_sites_top100_users.reset_index(drop=True)
 
-            return_dict["df_sites_top100_users_style"] = df_sites_top100_users.style.format({"reputation_ratio": "{:,.2%}"}).set_table_attributes(
+            return_dict["df_sites_top100_users_style"] = df_sites_top100_users.style.format(
+                {"reputation_ratio": "{:,.2%}"}).set_table_attributes(
                 'class="table table-sm table-striped table-hover table-responsive"').render()
             return_dict["df_sites_top100_users"] = df_sites_top100_users
             return_dict["items"] = list_dict_sites
@@ -72,7 +85,7 @@ class StackOverflow:
                 return_dict["has_more"] = r_json["has_more"]
             else:
                 return_dict["has_more"] = r_json["has_more"]
-
+            self.so_current_page_sites_dict = return_dict
             return return_dict
 
     def get_top_users_reputation(self, site, page, pagesize=SO_DEFAULT_PAGESIZE):
@@ -81,7 +94,7 @@ class StackOverflow:
         if isinstance(page, int):
             so_params_dict["page"] = page
         if isinstance(pagesize, int):
-            so_params_dict["pagesize"] = pagesize  # Get the top 100
+            so_params_dict["pagesize"] = pagesize
 
         so_params_dict["site"] = site
 
@@ -90,13 +103,23 @@ class StackOverflow:
             return {"error": r.status_code,
                     "url": {"method": self.so_api_url + self.so_url_users_reputation, "params": so_params_dict}}
         else:
+            return_dict = {}
+            df_site_top100_users = None
+            if self.so_current_page_sites_dict is not None:
+                df = self.so_current_page_sites_dict["df_sites_top100_users"]
+                df_site_top100_users = df.loc[df["site"] == site]
+                return_dict["df_site_top100_users"] = df_site_top100_users
+                # print(self.so_current_page_sites_dict)
+                # print("BBBBBB")
+                # print(return_dict["df_site_top100_users"])
+
             r_json = r.json()
             item_list = r_json["items"]
             list_dict_users = []
-            return_dict = {}
             for item in item_list[:pagesize]:
                 list_dict_users.append(
                     {"name": item["display_name"], "link": item["link"], "reputation": item["reputation"]})
+                # "reputation_ratio": df_site_top100_users.loc[df_site_top100_users['name'] == item["display_name"], "reputation_ratio"].iloc[0]})
             return_dict["items"] = list_dict_users
             return_dict["length"] = len(list_dict_users)
             return_dict["page"] = page
